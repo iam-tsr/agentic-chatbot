b7bb6f2183221d9bff2089925d927639e232a075
iam-tsr <tusharsoni.info@gmail.com>
2025-07-06 22:51:37 +0530

    LLM files - Gemini

---
+++ src/langgraph_multiAgent/utils/gemini_o2.py
@@ -0,0 +1,12 @@
+from llama_index.llms.google_genai import GoogleGenAI
+from llama_index.embeddings.gemini import GeminiEmbedding
+from llama_index.core import Settings
+
+
+Settings.llm = GoogleGenAI(
+    model="gemini-2.0-flash"
+)
+
+Settings.embed_model = GeminiEmbedding(
+    model="models/gemini-embedding-exp-03-07"
+)
